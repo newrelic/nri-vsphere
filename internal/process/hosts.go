4be369bc45d72aee4260b9645b29cdbd58a04316
Guillermo Sanchez Gavier <gsanchez@newrelic.com>
2020-04-02 18:12:46 +0200

    mod host entity

--- internal/process/hosts.go
+++ internal/process/hosts.go
@@ -6,7 +6,6 @@ import (
 	"strings"
 
 	"github.com/newrelic/infra-integrations-sdk/data/metric"
-	"github.com/newrelic/infra-integrations-sdk/integration"
 	"github.com/newrelic/nri-vmware-vsphere/internal/load"
 	"github.com/vmware/govmomi/object"
 )
@@ -17,70 +16,60 @@ func createHostSamples(timestamp int64) {
 	// create new entities for each host
 	for _, dc := range load.Datacenters {
 		for _, host := range dc.Hosts {
-			// entityName := host.Summary.Config.Name + ":hypervisor"
+			hostConfigName := host.Summary.Config.Name
+			entityName := hostConfigName + ":host"
+			datacenterName := dc.Datacenter.Name
+			clusterName := dc.Clusters[host.Parent.Reference()].Name
+			//TODO if using vcenter
+			//ha-datacenter is the default name when api is esxi
+			if datacenterName != "ha-datacenter" {
+				// to avoid redundant names when the host doesn't belong to any cluster
+				if clusterName == hostConfigName {
+					entityName = datacenterName + ":" + entityName
+				} else {
+					entityName = datacenterName + ":" + clusterName + ":" + entityName
+				}
+			}
 
-			// if load.Args.DatacenterLocation != "" {
-			// 	entityName = load.Args.DatacenterLocation + ":" + entityName
-			// }
-
-			// entityName = strings.ToLower(entityName)
-			// entityName = strings.ReplaceAll(entityName, ".", "-")
-
-			// workingEntity := setEntity(entityName, "vmware") // default type instance
-			// workingEntity.SetInventoryItem("name", "value", fmt.Sprintf("%v:%d", entityName, timestamp))
-
-			// // create SystemSample metric set
-			// systemSampleMetricSet := workingEntity.NewMetricSet("SystemSample")
+			if load.Args.DatacenterLocation != "" {
+				entityName = load.Args.DatacenterLocation + ":" + entityName
+			}
+			entityName = strings.ToLower(entityName)
+			entityName = strings.ReplaceAll(entityName, ".", "-")
 
 			// bios uuid identifies the host unequivocally and is available from vcenter/host api
-			uuid := integration.IDAttribute{Key: "uuid", Value: host.Summary.Hardware.Uuid}
-			workingEntity, err := load.Integration.Entity(host.Summary.Config.Name, "host", uuid)
+			// uuid := integration.IDAttribute{Key: "uuid", Value: host.Summary.Hardware.Uuid}
+			workingEntity, err := load.Integration.Entity(entityName, "vsphere")
 			if err != nil {
 				load.Logrus.WithError(err).Error("failed to create entity")
 			}
 
-			hostName := strings.ToLower(host.Summary.Config.Name)
-
-			workingEntity.SetInventoryItem("name", "value", fmt.Sprintf("%v:%d", hostName, timestamp))
+			workingEntity.SetInventoryItem("name", "value", fmt.Sprintf("%v:%d", entityName, timestamp))
 
 			systemSampleMetricSet := workingEntity.NewMetricSet("VSphereHostSample")
 
-			checkError(systemSampleMetricSet.SetMetric("datacenterName", dc.Datacenter.ManagedEntity.Name, metric.ATTRIBUTE))
-
-			// cluster := dc.FindCluster(host.Reference())
-			cluster := dc.Clusters[host.Parent.Reference()]
-			checkError(systemSampleMetricSet.SetMetric("clusterName", cluster.Name, metric.ATTRIBUTE))
-
-			resourcePools := dc.FindResourcePool(cluster.Reference())
+			//TODO if vcenter
+			checkError(systemSampleMetricSet.SetMetric("datacenterName", datacenterName, metric.ATTRIBUTE))
+			checkError(systemSampleMetricSet.SetMetric("clusterName", clusterName, metric.ATTRIBUTE))
+			resourcePools := dc.FindResourcePool(host.Parent.Reference())
 			resourcePoolList := ""
 			for _, rp := range resourcePools {
-				resourcePoolList += rp.Name + ","
+				resourcePoolList += rp.Name + "|"
 			}
 			checkError(systemSampleMetricSet.SetMetric("resourcePoolNameList", resourcePoolList, metric.ATTRIBUTE))
-
 			datastoreList := ""
 			for _, ds := range host.Datastore {
-				datastoreList += dc.Datastores[ds].Name + ","
+				datastoreList += dc.Datastores[ds].Name + "|"
 			}
 			checkError(systemSampleMetricSet.SetMetric("datastoreNameList", datastoreList, metric.ATTRIBUTE))
+			if load.Args.DatacenterLocation != "" {
+				checkError(systemSampleMetricSet.SetMetric("datacenterLocation", load.Args.DatacenterLocation, metric.ATTRIBUTE))
+			}
+			checkError(systemSampleMetricSet.SetMetric("hypervisorHostName", hostConfigName, metric.ATTRIBUTE))
 
-			// defaults
-			checkError(systemSampleMetricSet.SetMetric("integration_version", load.IntegrationVersion, metric.ATTRIBUTE))
-			checkError(systemSampleMetricSet.SetMetric("integration_name", load.IntegrationName, metric.ATTRIBUTE))
-			checkError(systemSampleMetricSet.SetMetric("timestamp", timestamp, metric.GAUGE))
-			checkError(systemSampleMetricSet.SetMetric("instanceType", "vmware-hypervisor", metric.ATTRIBUTE))
-			checkError(systemSampleMetricSet.SetMetric("datacenterLocation", load.Args.DatacenterLocation, metric.ATTRIBUTE))
-			checkError(systemSampleMetricSet.SetMetric("type", "hypervisor", metric.ATTRIBUTE))
-			checkError(systemSampleMetricSet.SetMetric("hostname", hostName, metric.ATTRIBUTE))
-
-			// host system
-			checkError(systemSampleMetricSet.SetMetric("hostSystem", host.Self.Value, metric.ATTRIBUTE))
-			checkError(systemSampleMetricSet.SetMetric("hypervisorHostname", hostName, metric.ATTRIBUTE))
-
-			// vms
-			checkError(systemSampleMetricSet.SetMetric("configName", host.Summary.Config.Name, metric.ATTRIBUTE))
 			checkError(systemSampleMetricSet.SetMetric("vmCount", len(host.Vm), metric.GAUGE))
 
+			//TODO change to list | and add map of networks
 			for i, nw := range host.Network {
 
 				network := object.NewNetwork(load.NetworkContainerView.Client(), nw)
@@ -89,35 +78,32 @@ func createHostSamples(timestamp int64) {
 					checkError(systemSampleMetricSet.SetMetric(fmt.Sprintf("network.%d", i), hostNetwork, metric.ATTRIBUTE))
 				}
 			}
-
-			// SystemSample metrics
-
-			// // memory
+			// memory
 			memoryTotalBytes := float64(host.Summary.Hardware.MemorySize)
-			checkError(systemSampleMetricSet.SetMetric("memoryTotalBytes", memoryTotalBytes, metric.GAUGE))
-			checkError(systemSampleMetricSet.SetMetric("systemMemoryBytes", memoryTotalBytes, metric.GAUGE))
-			memoryUsedBytes := float64(host.Summary.QuickStats.OverallMemoryUsage) * 1e+6
-			memoryFreeBytes := memoryTotalBytes - memoryUsedBytes
-			checkError(systemSampleMetricSet.SetMetric("memoryUsedBytes", memoryUsedBytes, metric.GAUGE))
-			checkError(systemSampleMetricSet.SetMetric("memoryFreeBytes", memoryFreeBytes, metric.GAUGE))
+			checkError(systemSampleMetricSet.SetMetric("mem.size", memoryTotalBytes, metric.GAUGE))
 
-			// // cpu
+			memoryUsedBytes := float64(host.Summary.QuickStats.OverallMemoryUsage) * 1e+6
+			checkError(systemSampleMetricSet.SetMetric("mem.usage", memoryUsedBytes, metric.GAUGE))
+
+			memoryFreeBytes := memoryTotalBytes - memoryUsedBytes
+			checkError(systemSampleMetricSet.SetMetric("mem.free", memoryFreeBytes, metric.GAUGE))
+
+			// cpu
 			CPUCores := host.Summary.Hardware.NumCpuCores
-			checkError(systemSampleMetricSet.SetMetric("coreCount", CPUCores, metric.GAUGE))
+			checkError(systemSampleMetricSet.SetMetric("cpu.cores", CPUCores, metric.GAUGE))
 
 			CPUThreads := host.Summary.Hardware.NumCpuThreads
-			checkError(systemSampleMetricSet.SetMetric("cpuThreads", CPUThreads, metric.GAUGE))
-
-			checkError(systemSampleMetricSet.SetMetric("overallCpuUsageMHz", host.Summary.QuickStats.OverallCpuUsage, metric.GAUGE))
+			checkError(systemSampleMetricSet.SetMetric("cpu.threads", CPUThreads, metric.GAUGE))
 
 			CPUMhz := host.Summary.Hardware.CpuMhz
-			checkError(systemSampleMetricSet.SetMetric("cpuMHz", CPUMhz, metric.GAUGE))
+			checkError(systemSampleMetricSet.SetMetric("cpu.coreMHz", CPUMhz, metric.GAUGE))
 
 			TotalMHz := float64(CPUMhz) * float64(CPUCores)
-			checkError(systemSampleMetricSet.SetMetric("totalMHz", TotalMHz, metric.GAUGE))
+			checkError(systemSampleMetricSet.SetMetric("cpu.totalMHz", TotalMHz, metric.GAUGE))
 
 			cpuPercent := (float64(host.Summary.QuickStats.OverallCpuUsage) / TotalMHz) * 100
-			checkError(systemSampleMetricSet.SetMetric("cpuPercent", cpuPercent, metric.GAUGE))
+			checkError(systemSampleMetricSet.SetMetric("cpu.percent", cpuPercent, metric.GAUGE))
+			checkError(systemSampleMetricSet.SetMetric("cpu.overallUsage", host.Summary.QuickStats.OverallCpuUsage, metric.GAUGE))
 
 			// disk
 			diskTotalBytes := int64(0)
@@ -129,8 +115,7 @@ func createHostSamples(timestamp int64) {
 					}
 				}
 			}
-
-			checkError(systemSampleMetricSet.SetMetric("diskTotalBytes", diskTotalBytes, metric.GAUGE))
+			checkError(systemSampleMetricSet.SetMetric("disk.TotalBytes", diskTotalBytes, metric.GAUGE))
 
 		}
 	}
