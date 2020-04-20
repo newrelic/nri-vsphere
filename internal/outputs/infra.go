4320f98e3acd974dc9783043d5f840ce402c5f2e
paologallinaharbur <paologallina1992@gmail.com>
2020-04-20 12:58:38 +0200

    feat (license, ReadMe):Added License, README and correcting minorbugs

--- internal/outputs/infra.go
+++ internal/outputs/infra.go
@@ -1,7 +1,5 @@
-/*
-* Copyright 2019 New Relic Corporation. All rights reserved.
-* SPDX-License-Identifier: Apache-2.0
- */
+// Copyright 2020 New Relic Corporation. All rights reserved.
+// SPDX-License-Identifier: Apache-2.0
 
 package outputs
 
@@ -29,4 +27,3 @@ func InfraIntegration(config *load.Config) error {
 	}
 	return nil
 }
-
