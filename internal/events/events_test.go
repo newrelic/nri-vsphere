78e4c240cff1e94090d13e15a3596778b9219d9a
Guillermo Sanchez Gavier <gsanchez@newrelic.com>
2021-11-16 13:56:20 +0100

    chore(deps): bump govmomi to v0.27

--- internal/events/events_test.go
+++ internal/events/events_test.go
@@ -8,64 +8,37 @@ import (
 
 	logrus "github.com/sirupsen/logrus"
 	"github.com/stretchr/testify/assert"
-	"github.com/vmware/govmomi"
 	"github.com/vmware/govmomi/simulator"
-	"github.com/vmware/govmomi/view"
-	"github.com/vmware/govmomi/vim25/mo"
+	"github.com/vmware/govmomi/vim25"
 )
 
 func TestEvents(t *testing.T) {
-
-	ctx := context.Background()
-
-	//SettingUp Simulator
 	model := simulator.VPX()
-	model.Machine = 5
-	defer model.Remove()
-	err := model.Create()
-	if err != nil {
-		logrus.Fatal(err)
-	}
-	s := model.Service.NewServer()
+	simulator.Test(func(ctx context.Context, vc *vim25.Client) {
+		ca := NewCacheMock{
+			TimestampCache: time.Now().Add(-15 * time.Second),
+		}
 
-	c, _ := govmomi.NewClient(ctx, s.URL, true)
+		// A virtual machine contains 6 events on the simulator.
+		ref := simulator.Map.Any("VirtualMachine").Reference()
 
-	var datacenters []mo.Datacenter
+		// https://pubs.vmware.com/vsphere-51/index.jsp?topic=%2Fcom.vmware.wssdk.apiref.doc%2Fvim.HistoryCollector.html
+		ed, err := NewEventDispacher(vc, ref, logrus.New(), &ca)
+		assert.NoError(t, err)
 
-	m := view.NewManager(c.Client)
+		ed.CollectEvents("5")
+		assert.Equal(t, 6, len(ed.Events), "We were expecting 6 events")
+		ed.Cancel()
 
-	cv, err := m.CreateContainerView(ctx, c.ServiceContent.RootFolder, []string{"Datacenter"}, true)
-	if err != nil {
-		logrus.WithError(err).Fatal("failed to create Datacenter container view")
-	}
-	err = cv.Retrieve(ctx, []string{"Datacenter"}, []string{"name", "overallStatus"}, &datacenters)
-	assert.NoError(t, err)
-
-	ca := NewCacheMock{}
-
-	//https://pubs.vmware.com/vsphere-51/index.jsp?topic=%2Fcom.vmware.wssdk.apiref.doc%2Fvim.HistoryCollector.html
-	ed, err := NewEventDispacher(c.Client, datacenters[0].Reference(), logrus.New(), &ca)
-	assert.NoError(t, err)
-
-	ed.CollectEvents("5")
-	assert.Equal(t, 10, len(ed.Events), "We were expecting 10 events")
-	ed.Cancel()
-
-	ed2, err := NewEventDispacher(c.Client, datacenters[0].Reference(), logrus.New(), &ca)
-	assert.NoError(t, err)
-	ed2.CollectEvents("noParsable")
-	assert.Equal(t, 10, len(ed2.Events), "We were expecting 10 events")
-	ed2.Cancel()
-
-	ed3, err := NewEventDispacher(c.Client, datacenters[0].Reference(), logrus.New(), &ca)
-	assert.NoError(t, err)
-	ed3.CollectEvents("3")
-	assert.Equal(t, 10, len(ed.Events), "We were expecting 10 events")
-	ed3.Cancel()
-
+		ed.CollectEvents("noParsable")
+		assert.Equal(t, 6, len(ed.Events), "We were expecting 6 events")
+		ed.Cancel()
+	}, model)
 }
 
-type NewCacheMock struct{}
+type NewCacheMock struct {
+	TimestampCache time.Time
+}
 
 func TestSanitizeTimestamp(t *testing.T) {
 	now := time.Now()
@@ -87,13 +60,12 @@ func TestSanitizeTimestamp(t *testing.T) {
 
 	s = sanitizeTimestamp(nil, log, last, now)
 	assert.Equal(t, last.Add(time.Duration(1)*time.Second), s)
-
 }
 
-func (*NewCacheMock) ReadTimestampCache() (time.Time, error) {
-	return time.Now(), nil
+func (c *NewCacheMock) ReadTimestampCache() (time.Time, error) {
+	return c.TimestampCache, nil
 }
 
-func (*NewCacheMock) WriteTimestampCache(t time.Time) error {
+func (c *NewCacheMock) WriteTimestampCache(t time.Time) error {
 	return nil
 }
